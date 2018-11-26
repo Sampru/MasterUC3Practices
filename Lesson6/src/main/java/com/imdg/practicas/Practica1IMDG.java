5816c47495def2540fcdd6e08b6afeae6fbae3ce
Sampru <sampru_96@hotmail.com>
2018-11-26 16:06:39 +0100

    Lesson 6 completed

--- Lesson6/src/main/java/com/imdg/practicas/Practica1IMDG.java
+++ Lesson6/src/main/java/com/imdg/practicas/Practica1IMDG.java
@@ -4,22 +4,20 @@ import com.hazelcast.config.Config;
 import com.hazelcast.core.Hazelcast;
 import com.hazelcast.core.HazelcastInstance;
 import com.hazelcast.core.IMap;
-import com.imdg.pojos.Person;
-
-import java.util.Map;
-import java.util.Queue;
 
 public class Practica1IMDG {
 
     public static void main(String[] args) {
-	    // Instanciar hazelcast y crear una cache
-        // Insertar un dato y arrancar 3 veces el main,
-        // Leer el output de consola y ver como hazelcast va encontrando "miembros"
-        // Comprobar que se conectan (en el output deberian verse 3 miembros en la consola) y capturarlo
+        // Configurar la red para uso local
         Config config = new Config();
         config.getNetworkConfig().getJoin().getTcpIpConfig().addMember("localhost").setEnabled(true);
         config.getNetworkConfig().getJoin().getMulticastConfig().setEnabled(false);
 
+        // Instanciar hazelcast y crear una cache
+        HazelcastInstance haze = Hazelcast.newHazelcastInstance();
+        IMap<String, String> cache = haze.getMap("Lesson6.1");
 
+        // Insertar un dato
+        cache.put("Key", "Value");
     }
 }
---
+++ Lesson6/src/main/java/com/imdg/practicas/Practica2IMDGc.java
@@ -0,0 +1,58 @@
+package com.imdg.practicas;
+
+import com.hazelcast.config.Config;
+import com.hazelcast.core.Hazelcast;
+import com.hazelcast.core.HazelcastInstance;
+import com.hazelcast.core.ICountDownLatch;
+import com.hazelcast.core.IMap;
+import com.imdg.pojos.Person;
+
+import java.util.concurrent.TimeUnit;
+
+public class Practica2IMDGc {
+
+
+    public static void main(String[] args) {
+        // Configurar la red para uso local
+        Config config = new Config();
+        config.getNetworkConfig().getJoin().getTcpIpConfig().addMember("localhost").setEnabled(true);
+        config.getNetworkConfig().getJoin().getMulticastConfig().setEnabled(false);
+
+        // Crear persona
+        Person person = new Person("Elena Nillo", 28053, "Jerusalem", "Deus vult!");
+
+        // Instanciar hazelcast y crear una cache
+        HazelcastInstance haze = Hazelcast.newHazelcastInstance();
+        IMap<String, Person> cache = haze.getMap("Lesson6.2");
+
+        // Crear un latch
+        ICountDownLatch latch = haze.getCountDownLatch("Lesson6.2");
+
+        try {
+            // Darle 3 tokens al latch
+            latch.trySetCount(3);
+
+            // Insertar un dato
+            cache.put("nodo3", person);
+
+            // Quitar un token
+            latch.countDown();
+            System.out.println(latch.getCount() + " nodos restantes.");
+
+            // Esperar a demas procesos
+            latch.await(100, TimeUnit.SECONDS);
+
+            // Leer los 3 datos.
+            System.out.println(cache.get("nodo1").toString());
+            System.out.println(cache.get("nodo2").toString());
+            System.out.println(cache.get("nodo3").toString());
+
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            latch.destroy();
+        }
+
+
+    }
+}
